4938c04823a37904cdb37b98261138c784e7a63c
Aditya Narayan <narayanaditya1007@gmail.com>
2024-04-11 18:43:59 +0530

    remove email redundancy

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -33,6 +33,12 @@ async function login(req,res){
 async function signup(req,res){
     try{
         console.log("hello");
+        const existUser = await User.findOne({email:req.body.email});
+        if(existUser){
+            console.log("user exists");
+            res.send("Email already exists, Try loggin in")
+            throw new Error("Email already exists, Try loggin in")
+        }
         const hashPass = await bcrypt.hash(req.body.password,10);
         const user = new User({
             name : req.body.name,
